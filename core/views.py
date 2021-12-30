e6f425cc9b7bbd05510f4740b96f27b09635c82f
Himanshu Dhiman <45459993+himdhiman@users.noreply.github.com>
2021-12-30 10:43:03 +0530

    updated

--- core/views.py
+++ core/views.py
@@ -42,29 +42,27 @@ class RunCode(APIView):
     permission_classes = (permissions.AllowAny, )
     def post(self, request):
         body = json.loads(request.body)
-        # headers = request.headers.get("Authorization")
-        # mail_id = None
-        # if not headers:
-        #     return Response(status = status.HTTP_401_UNAUTHORIZED)
-        # else:
-        #     access_token = headers.split(' ')[1]
-        #     response = middleware.Authentication.isAuthenticated(access_token)
-        #     if not response["success"]:
-        #         return Response(status = status.HTTP_401_UNAUTHORIZED)
-        #     else:
-        #         mail_id = response['data']['email']
-        # if not mail_id:
-        #     return Response(status = status.HTTP_401_UNAUTHORIZED)
+        headers = request.headers.get("Authorization")
+        mail_id = None
+        if not headers:
+            return Response(status = status.HTTP_401_UNAUTHORIZED)
+        else:
+            access_token = headers.split(' ')[1]
+            response = middleware.Authentication.isAuthenticated(access_token)
+            if not response["success"]:
+                return Response(status = status.HTTP_401_UNAUTHORIZED)
+            else:
+                mail_id = response['data']['email']
+        if not mail_id:
+            return Response(status = status.HTTP_401_UNAUTHORIZED)
 
-        # body["created_By"] = mail_id
-        body["created_By"] = "server@gmail.com"
+        body["created_By"] = mail_id
 
         arr1 = body["created_By"].split("@")
         arr2 = arr1[1].split(".")
         uid = arr1[0] + arr2[0] + arr2[1]
 
         context = {"body" : body, "uid" : uid}
-        print(context["uid"])
         tasks.runCode.delay(context)
         return Response(status = status.HTTP_202_ACCEPTED)
 
