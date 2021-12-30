e393f4a7ca5d20add0571957ca73e9ba5679668b
Himanshu Dhiman <45459993+himdhiman@users.noreply.github.com>
2021-12-30 14:59:21 +0530

    updated

--- problems/admin.py
+++ problems/admin.py
@@ -1,10 +1,49 @@
 from django.contrib import admin
 from problems import models
 
+
+class submissionAdmin(admin.ModelAdmin):
+    list_display = ("created_By", "problem_name", "language", "status", "submission_Date_Time")
+
+    def problem_name(self, obj):
+        prob_obj = models.Problem.objects.get(id = obj.problem_Id)
+        return prob_obj.title
+
+class editorialAdmin(admin.ModelAdmin):
+    list_display = ("problem_name", "CPP17", "Java", "Python3", )
+
+    def problem_name(self, obj):
+        prob_obj = models.Problem.objects.get(id = obj.problem_Id)
+        return prob_obj.title
+
+    def CPP17(self, obj):
+        if obj.cpp17 != "":
+            return True
+        return False
+
+    def Java(self, obj):
+        if obj.java != "":
+            return True
+        return False
+
+    def Python3(self, obj):
+        if obj.python3 != "":
+            return True
+        return False
+
+    CPP17.boolean = True
+    Java.boolean = True
+    Python3.boolean = True
+
+
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_by", "problem_level", "approved_by_admin", )
+
+
 admin.site.register(models.Tag)
 admin.site.register(models.Bookmark)
-admin.site.register(models.Problem)
-admin.site.register(models.Submission)
+admin.site.register(models.Problem, ProblemAdmin)
+admin.site.register(models.Submission, submissionAdmin)
 admin.site.register(models.UpvotesDownvote)
-admin.site.register(models.Editorial)
+admin.site.register(models.Editorial, editorialAdmin)
 
